70bd1aff4050b7384dfdf818b853bb00c5e6c195
Mohammed Alom <mohammed_alom@mcafee.com>
2019-07-17 20:37:41 +0100

    remove indentation

--- frontend/src/app/message/message.component.ts
+++ frontend/src/app/message/message.component.ts
@@ -4,12 +4,10 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-message',
   template: `This is the message component 
             <div *ngFor="let message of messages">
-            <mat-card>
-              <mat-card-header>
-              <mat-card-title>{{message.owner}}</mat-card-title> 
-              </mat-card-header>
-              <mat-card-content>
-              {{message.text}}  
+            <mat-card>            
+                <mat-card-title>{{message.owner}}</mat-card-title>  
+            <mat-card-content>
+                {{message.text}}  
               </mat-card-content>
             </mat-card>                   
             </div>`
